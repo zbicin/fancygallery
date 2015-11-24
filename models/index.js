df39c58515f1af3e4e666ecc65c42440330d4117
Krzysztof Zbiciński <fistasheq+github@gmail.com>
2015-11-24 22:41:08 +0100

    adjusting project to make openshift happy

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable] + config.database);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
